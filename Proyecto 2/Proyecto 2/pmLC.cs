8480200f419cec075e95ba2fc5ae5c4b23a77dee
RAF28gitH <raf28elnooob@gmail.com>
2024-10-24 08:12:12 -0600

    Creé un archivo para poner ahí mi materia :D

---
+++ Proyecto 2/Proyecto 2/pmLC.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC49
+ * Date: 24/10/2024
+ * Time: 08:09 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Proyecto_2
+{
+	/// <summary>
+	/// Description of pmLC.
+	/// </summary>
+	public partial class pmLC : Form
+	{
+		public pmLC()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
